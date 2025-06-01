f4b580ebe47ea0b94eac2309d0abe0696d7b42c5
p-and14 <p-and14@yandex.ru>
2025-06-01 12:50:45 +0300

    first commit

---
+++ src/models.py
@@ -0,0 +1,144 @@
+from datetime import datetime, date
+import enum
+from typing import Annotated
+from uuid import uuid4, UUID
+
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column, relationship
+from sqlalchemy import String, Text, ForeignKey
+from sqlalchemy.sql import func
+
+
+uuidpk = Annotated[UUID, mapped_column(primary_key=True, default=uuid4)]
+str_100 = Annotated[str, mapped_column(String(100))]
+
+
+class Base(DeclarativeBase):
+    repr_cols_num = 3
+    repr_cols = tuple()
+        
+    def __repr__(self):
+        cols = []
+        for idx, col in enumerate(self.__table__.columns.keys()):
+            if col in self.repr_cols or idx < self.repr_cols_num:
+                cols.append(f"{col}={getattr(self, col)}")
+
+        return f"<{self.__class__.__name__} {', '.join(cols)}>"
+    
+
+class User(Base):
+    __tablename__ = "user"
+
+    id: Mapped[uuidpk]
+    full_name: Mapped[str_100]
+    email: Mapped[str] = mapped_column(String(120), unique=True)
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=func.now()
+    )
+    created_tasks: Mapped[list["Task"]] = relationship(
+        back_populates="author",
+        primaryjoin="User.id == Task.author_id"
+    )
+    assigned_tasks: Mapped[list["Task"]] = relationship(
+        back_populates="assignee",
+        primaryjoin="User.id == Task.assignee_id"
+        )
+    participant_tasks: Mapped[list["Task"]] = relationship(
+        back_populates="participants",
+        secondary="task_participant"
+    )
+    
+
+class Task(Base):
+    __tablename__ = "task"
+    repr_cols_num = 4
+
+    class Status(enum.Enum):
+        todo = "todo"
+        in_progress = "in_progress"
+        done = "done"
+
+    id: Mapped[uuidpk]
+    title: Mapped[str] = mapped_column(String(255))
+    description: Mapped[str | None] = mapped_column(Text)
+    status: Mapped[Status]
+    created_at: Mapped[datetime] = mapped_column(
+        server_default=func.now(),
+        index=True)
+    author_id: Mapped[UUID] = mapped_column(ForeignKey("user.id", ondelete="CASCADE"))
+    author: Mapped["User"] = relationship(
+        back_populates="created_tasks",
+        primaryjoin="Task.author_id == User.id"
+    )
+    assignee_id: Mapped[UUID | None] = mapped_column(ForeignKey("user.id", ondelete="SET NULL"))
+    assignee: Mapped["User"] = relationship(
+        back_populates="assigned_tasks",
+        primaryjoin="Task.assignee_id == User.id"
+    )
+    participants: Mapped[list["User"]] = relationship(
+        back_populates="participant_tasks",
+        secondary="task_participant"
+    )
+    column_id: Mapped[UUID | None] = mapped_column(ForeignKey("column.id", ondelete="SET NULL"))
+    column: Mapped["Column | None"] = relationship(back_populates="tasks")
+    sprint_id: Mapped[UUID | None] = mapped_column(ForeignKey("sprint.id", ondelete="SET NULL"))
+    sprint: Mapped["Sprint | None"] = relationship(back_populates="tasks")
+    board_id: Mapped[UUID | None] = mapped_column(ForeignKey("board.id", ondelete="SET NULL"))
+    board: Mapped["Board | None"] = relationship(back_populates="tasks")
+    group_id: Mapped[UUID | None] = mapped_column(ForeignKey("group.id", ondelete="SET NULL"))
+    group: Mapped["Group | None"] = relationship(back_populates="tasks")
+
+
+class TaskParticipant(Base):
+    __tablename__ = "task_participant"
+
+    class Role(enum.Enum):
+        executor = "executor"
+        watcher = "watcher"
+
+    id: Mapped[uuidpk]
+    user_id: Mapped[UUID] = mapped_column(
+        ForeignKey("user.id", ondelete="CASCADE"),
+        primary_key=True
+    )
+    task_id: Mapped[UUID] = mapped_column(
+        ForeignKey("task.id", ondelete="CASCADE"),
+        primary_key=True
+    )
+    role: Mapped[Role]
+
+
+class Board(Base):
+    __tablename__ = "board"
+
+    id: Mapped[uuidpk]
+    name: Mapped[str_100] = mapped_column(unique=True)
+    tasks: Mapped[list["Task"]] = relationship(back_populates="board")
+    columns: Mapped[list["Column"]] = relationship(back_populates="board")
+
+
+class Column(Base):
+    __tablename__ = "column"
+
+    id: Mapped[uuidpk]
+    name: Mapped[str] = mapped_column(String(100), unique=True)
+    board_id: Mapped[UUID] = mapped_column(ForeignKey("board.id", ondelete="CASCADE"))
+    board: Mapped["Board"] = relationship(back_populates="columns")
+    tasks: Mapped[list["Task"]] = relationship(back_populates="column")
+
+
+class Sprint(Base):
+    __tablename__ = "sprint"
+
+    id: Mapped[uuidpk]
+    name: Mapped[str_100]
+    start_date: Mapped[date]
+    end_date: Mapped[date]
+    tasks: Mapped[list["Task"]] = relationship(back_populates="sprint")
+
+
+class Group(Base):
+    __tablename__ = "group"
+
+    id: Mapped[uuidpk]
+    name: Mapped[str_100]
+    tasks: Mapped[list["Task"]] = relationship(back_populates="group")
